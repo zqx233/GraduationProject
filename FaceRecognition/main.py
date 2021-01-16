e2387fc9ddccf9f75707cc2aadc6cb1435450293
zhaoq <zhaoqixinmain@qq.com>
2021-01-16 21:26:49 +0800

    完成了主页的buttons

--- FaceRecognition/main.py
+++ FaceRecognition/main.py
@@ -1,20 +1,41 @@
 import tkinter as tk
 
 from aip import AipOcr
-from GUI import Widgets
+from GUI import AppWidgets
+
+root = tk.Tk()
+root.title('人脸识别系统')
 
 
 def main():
-    root = Widgets.Windows(None, None, None, '人脸识别系统V0.1').root_window()
-    f1 = Widgets.Frames(root, 100, 300, 5, 5, None)
-    f1.frames()
-    f1.postion(0, 0)
-    b1 = Widgets.Buttons(f1, 5, 10, 5, 5, 'ok', lambda: print('ok'))
-    b1.buttons()
-    b1.postion(0, 0)
 
-    # face_detection = Widgets.Windows(root, 500, 500, '人脸检测').toplevel_windows()
-    tk.mainloop()
+
+    fun_button1 = AppWidgets.AllButtons(root, '人脸识别与属性分析', lambda: ceate_window('人脸识别与属性分析'), )
+    fun_button1.main_buttons(20, 10)
+    fun_button1.postion(2, 0)
+
+    fun_button2 = AppWidgets.AllButtons(root, '人脸对比', lambda: ceate_window('人脸对比'), )
+    fun_button2.main_buttons(20, 10)
+    fun_button2.postion(2, 1)
+
+    fun_button3 = AppWidgets.AllButtons(root, '人脸搜索', lambda: ceate_window('人脸搜索'), )
+    fun_button3.main_buttons(20, 10)
+    fun_button3.postion(2, 2)
+
+    fun_button4 = AppWidgets.AllButtons(root, 'ok', lambda: ceate_window('功能2'), )
+    fun_button4.main_buttons(20, 10)
+    fun_button4.postion(2, 3)
+
+    fun_button5 = AppWidgets.AllButtons(root, 'ok', lambda: ceate_window('关于'), )
+    fun_button5.main_buttons(20, 10)
+    fun_button5.postion(2, 4)
+
+    root.mainloop()
+
+
+def ceate_window(title):
+    w1 = AppWidgets.AllWindows(root, title)
+    w1.top_windows()
 
 
 if __name__ == '__main__':
