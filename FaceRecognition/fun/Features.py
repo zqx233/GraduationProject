bc8e0d358cd5b7cf434873205f2c72202150d898
zhaoq <zhaoqixinmain@qq.com>
2021-01-18 21:48:51 +0800

    重写了代码，之前写的遇到了解决不了的问题

--- FaceRecognition/fun/Features.py
+++
@@ -1,8 +0,0 @@
-from FaceRecognition.fun import AppWidgets
-
-
-def create_window(master, title):
-    w = AppWidgets.AllWindows(master, title).top_windows()
-    img_label1 = AppWidgets.ImgLabel(AppWidgets.AllWindows.master, None, None)
-    img_label1.label(image='./src/img/face.png')
-    img_label1.location(0, 0, 5, 5, 1, 'n')
--- FaceRecognition/main.py
+++ FaceRecognition/main.py
@@ -1,42 +1,102 @@
-import tkinter as tk
-
-from aip import AipOcr
-from fun import AppWidgets, Features
-
-root = tk.Tk()
-root.title('人脸识别系统V0.1')
-# root.geometry('1250x550')
-# root.resizable(width=False, height=False)
+from tkinter import *
 
 
-def main():
-    main_label = AppWidgets.AllLabels(root, '人脸识别系统', ('', 18))
-    main_label.label()
-    main_label.location(1, 1, 5, 20, 3, 'n' + 's')
+class Home(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.grid()
+        # 标题组件
+        self.title_label = Label(self.master, text='人脸识别系统', font=('', 20))
+        # 按钮组件图标
+        self.fun1_img = PhotoImage(file='src/img/face.png')
+        self.fun2_img = PhotoImage(file='src/img/compare.png')
+        self.fun3_img = PhotoImage(file='src/img/search.png')
+        self.fun4_img = PhotoImage(file='src/img/detect.png')
+        self.fun5_img = PhotoImage(file='src/img/about.png')
+        # 按钮组件
+        self.fun1_but = Button(self.master, width=150, height=150, text='人脸识别与属性分析', font=('', 12), image=self.fun1_img,
+                               compound='top')
+        self.fun2_but = Button(self.master, width=150, height=150, text='人脸对比', font=('', 12), image=self.fun2_img,
+                               compound='top')
+        self.fun3_but = Button(self.master, width=150, height=150, text='人脸搜索', font=('', 12), image=self.fun3_img,
+                               compound='top')
+        self.fun4_but = Button(self.master, width=150, height=150, text='图片活体检测', font=('', 12), image=self.fun4_img,
+                               compound='top')
+        self.fun5_but = Button(self.master, width=150, height=150, text='关于', font=('', 12), image=self.fun5_img,
+                               compound='top')
+        # 布局
+        self.title_label.grid(row=1, column=1, padx=5, pady=20, columnspan=3)
+        self.fun1_but.grid(row=2, column=0, padx=10, pady=25)
+        self.fun2_but.grid(row=2, column=1, padx=10, pady=25)
+        self.fun3_but.grid(row=2, column=2, padx=10, pady=25)
+        self.fun4_but.grid(row=2, column=3, padx=10, pady=25)
+        self.fun5_but.grid(row=2, column=4, padx=10, pady=25)
+        # 按键绑定
+        self.fun1_but.bind('<Button-1>', self.fun1_but_ev)
+        self.fun2_but.bind('<Button-1>', self.fun2_but_ev)
+        self.fun3_but.bind('<Button-1>', self.fun3_but_ev)
+        self.fun4_but.bind('<Button-1>', self.fun4_but_ev)
+        self.fun5_but.bind('<Button-1>', self.fun5_but_ev)
 
-    fun_button1 = AppWidgets.AllButtons(root, '人脸识别与属性分析', lambda: Features.create_window(root, '人脸识别与属性分析'), )
-    fun_button1.main_buttons(150, 150, 'src/img/face.png')
-    fun_button1.location(2, 0)
+    def fun1_but_ev(self, event):
+        Face(master=self.master)
 
-    # fun_button2 = AppWidgets.AllButtons(root, '人脸对比', lambda: Features.create_window(root, '人脸对比'), )
-    # fun_button2.main_buttons(150, 150, 'src/img/compare.png')
-    # fun_button2.location(2, 1)
-    #
-    # fun_button3 = AppWidgets.AllButtons(root, '人脸搜索', lambda: Features.create_window(root, '人脸搜索'), )
-    # fun_button3.main_buttons(150, 150, 'src/img/search.png')
-    # fun_button3.location(2, 2)
-    #
-    # fun_button4 = AppWidgets.AllButtons(root, '图片活体检测', lambda: Features.create_window(root, '图片活体检测'), )
-    # fun_button4.main_buttons(150, 150, 'src/img/detect.png')
-    # fun_button4.location(2, 3)
-    #
-    # fun_button5 = AppWidgets.AllButtons(root, '关于', lambda: Features.create_window(root, '关于'), )
-    # fun_button5.main_buttons(150, 150, 'src/img/about.png')
-    # fun_button5.location(2, 4)
+    def fun2_but_ev(self, event):
+        Face(master=self.master)
+
+    def fun3_but_ev(self, event):
+        Face(master=self.master)
+
+    def fun4_but_ev(self, event):
+        Face(master=self.master)
+
+    def fun5_but_ev(self, event):
+        Face(master=self.master)
 
 
-    root.mainloop()
+class Face(Frame):
+    """功能1界面"""
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        # 功能1窗口
+        self.fun1_win = Toplevel(master=self.master)
+        self.fun1_win.title('人脸识别与属性分析')
+        self.grid()
+        # 识别结果
+        self.result_frame = LabelFrame(master=self.fun1_win, text='识别结果', width=50, height=50)
+        # 图片展示
+        self.img_frame = LabelFrame(master=self.fun1_win, text='图片')
+        self.up_img = PhotoImage(file='src/img/upload.png')
+        self.img_label = Label(self.img_frame, width=500, height=500, image=self.up_img)
+        # 上传图片以及参数
+        self.up_frame = LabelFrame(master=self.fun1_win, text='上传图片')
+        self.file_entry = Entry(self.up_frame, )
+        self.upload_but = Button(self.up_frame, text='确  定', )
+        self.live = IntVar()
+        self.live_check = Checkbutton(master=self.up_frame, text='图片活体检测', variable=self.live, )
+        self.params = ['年龄', '性别', '口罩']
+        self.v = []
+        i = 1
+        for param in self.params:
+            i=i+1
+            self.v.append(IntVar())
+            self.params_check = Checkbutton(master=self.up_frame, text=param, variable=self.v[-1])
+            self.params_check.grid(row=4, column=i)
+        # 布局
+        self.img_frame.grid(row=1, column=2, padx=5)
+        self.up_frame.grid(row=2, column=2, padx=5, sticky='w' + 'e')
+        self.result_frame.grid(row=1, column=3, padx=5)
+        self.img_label.grid(row=2, column=2)
+        self.file_entry.grid(row=3, column=2, padx=5, sticky='w' + 'e')
+        self.upload_but.grid(row=3, column=3, sticky='e')
 
 
 if __name__ == '__main__':
-    main()
+    root = Tk()
+    root.title('人脸识别系统V0.1')
+    root.geometry('1500x600')
+    Home(master=root)
+    root.mainloop()
